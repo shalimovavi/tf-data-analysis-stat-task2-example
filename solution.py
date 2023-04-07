9f588715e05f7a70ca403a4584d44cb47c707dd1
shalimovavi <45150630+shalimovavi@users.noreply.github.com>
2023-04-07 09:02:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,6 @@ chat_id = 1267315308 # Ваш chat ID, не меняйте название пе
 
 def solution(p: float, x: np.array) -> tuple:
     y = x - 0.059
-    return max(y)/(pow((1+p)/2,1/n)+0.059), \ max(y)/(pow((1-p)/2,1/n)+0.059)
+    n = len(x)
+    return max(y)/(pow((1+p)/2,1/n))+0.059, \
+    max(y)/(pow((1-p)/2,1/n))+0.059
